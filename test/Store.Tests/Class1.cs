f8375f5748f06f2573d37f62509299b3aa43cdc7
ppowierza <p.powierza91@gmail.com>
2020-01-25 18:45:33 +0100

    Add simple pagination, first test for pagination, streamlining

--- test/Store.Tests/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Store.Tests
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ test/Store.Tests/ProductControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.Controllers;
+using BookStore.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Store.Tests
+{
+    
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void CanPaginate()
+        {
+            //arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product{ProductID = 1, Name = "P1"},
+                new Product{ProductID = 2, Name = "P2"},
+                new Product{ProductID = 3, Name = "P3"},
+                new Product{ProductID = 4, Name = "P4"},
+                new Product{ProductID = 5, Name = "P5"},
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //Act
+            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+
+            //Assert
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+
+
+        }
+    }
+}
